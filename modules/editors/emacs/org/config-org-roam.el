781ab9b81d1c50be27582665ee8ea27bcf6036af
Regalk <72028266+regalk13@users.noreply.github.com>
2025-05-18 09:17:35 -0500

    feat: org-roam binds support

--- modules/editors/emacs/org/config-org-roam.el
+++ modules/editors/emacs/org/config-org-roam.el
@@ -1,4 +1,8 @@
 (use-package org-roam
+  :ensure t
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
   :config
   (setq org-roam-directory (file-truename "~/notes/org"))
   ;; (setq find-file-visit-truename t) ;; Resolve symlinks
