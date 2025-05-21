b5ca03519e97cfb335229ea0796613d1bdbf7a7b
Regalk <72028266+regalk13@users.noreply.github.com>
2025-05-20 19:58:16 -0500

    feat: back to default DLS

--- modules/desktop/digital-logic-sim.nix
+++ modules/desktop/digital-logic-sim.nix
@@ -4,6 +4,7 @@
 }:
 {
     environment.systemPackages = [
-        inputs.digital-logic-sim.packages.x86_64-linux.fork16bit
+        # inputs.digital-logic-sim.packages.x86_64-linux.fork16bit
+        inputs.digital-logic-sim.packages.x86_64-linux.default
     ];
 }
