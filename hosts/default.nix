112ec4f37294e3a59438cf34e40414a5302da8f2
Regalk <72028266+regalk13@users.noreply.github.com>
2025-05-27 20:58:04 -0500

    style: apply fmt to all modules

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,25 +1,24 @@
 { inputs, self, ... }:
 let
   mkHost = import ./lib/mkHost.nix { inherit inputs self; };
-  modulePath = ../modules;
 in
 {
   flake.nixosConfigurations = {
     morion = mkHost {
-        name          = "morion";
-        system        = "x86_64-linux";
-        extraImports  = [
-          ../modules/graphical/hyprland.nix
-          ../modules/cli/tools.nix
-          ../modules/core
-          ../modules/desktop/apps.nix
-          ../modules/desktop/fonts.nix
-          ../modules/networking/firewall.nix
-          ../modules/networking/ssh.nix
-          ../modules/networking/redis.nix
-          ../modules/hardware/audio.nix
-          ../modules/hardware/sensors.nix
-        ];
+      name = "morion";
+      system = "x86_64-linux";
+      extraImports = [
+        ../modules/graphical/hyprland.nix
+        ../modules/cli/tools.nix
+        ../modules/core
+        ../modules/desktop/apps.nix
+        ../modules/desktop/fonts.nix
+        ../modules/networking/firewall.nix
+        ../modules/networking/ssh.nix
+        ../modules/networking/redis.nix
+        ../modules/hardware/audio.nix
+        ../modules/hardware/sensors.nix
+      ];
     };
   };
 }
--- hosts/lib/mkHost.nix
+++ hosts/lib/mkHost.nix
@@ -1,10 +1,11 @@
 { inputs, self }:
 
-{ name
-, system ? "x86_64-linux"
-, username ? "regalk"
-, extraImports ? [ ]
-, extraSpecialArgs ? { }
+{
+  name,
+  system ? "x86_64-linux",
+  username ? "regalk",
+  extraImports ? [ ],
+  extraSpecialArgs ? { },
 }:
 
 let
@@ -20,20 +21,25 @@ let
     home-manager.nixosModules.home-manager
     # inputs.stylix.nixosModules.stylix
 
-    ({ config, ... }: {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = { inherit inputs system username; };
-        sharedModules   = [ (self + /home/default.nix) ];
-        users.${username} = import "${self}/users/${username}/home.nix";
-      };
-    })
+    (
+      _:
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = { inherit inputs system username; };
+          sharedModules = [ (self + /home/default.nix) ];
+          users.${username} = import "${self}/users/${username}/home.nix";
+        };
+      }
+    )
   ];
 
-in 
+in
 pkgs.lib.nixosSystem {
   inherit system;
-  specialArgs = { inherit inputs self username; } // extraSpecialArgs;
-  modules     = baseModules ++ extraImports;
+  specialArgs = {
+    inherit inputs self username;
+  } // extraSpecialArgs;
+  modules = baseModules ++ extraImports;
 }
